c13617074af82ba71840ae6dd8be1b484f09b04f
TarikUveysSen <104647805+TarikUveysSen@users.noreply.github.com>
2023-09-25 14:50:22 +0300

    Update derivative.py

--- derivative.py
+++ derivative.py
@@ -15,13 +15,24 @@ df['First_Derivative'] = df['Adj Close'].diff()
 # Calculate the second derivative (acceleration)
 df['Second_Derivative'] = df['First_Derivative'].diff()
 
-# Plot the first and second derivatives over time
+# Calculate moving averages (MA) for both derivatives (10-day and 50-day)
+df['First_Derivative_10_MA'] = df['First_Derivative'].rolling(window=10).mean()
+df['First_Derivative_50_MA'] = df['First_Derivative'].rolling(window=50).mean()
+df['Second_Derivative_10_MA'] = df['Second_Derivative'].rolling(window=10).mean()
+df['Second_Derivative_50_MA'] = df['Second_Derivative'].rolling(window=50).mean()
+
+# Plot the first and second derivatives and their MAs over time
 plt.figure(figsize=(12, 6))
 plt.plot(df.index, df['First_Derivative'], label='First Derivative', color='blue')
 plt.plot(df.index, df['Second_Derivative'], label='Second Derivative', color='green')
+plt.plot(df.index, df['First_Derivative_10_MA'], label='First Derivative 10-day MA', linestyle='--', color='orange')
+plt.plot(df.index, df['First_Derivative_50_MA'], label='First Derivative 50-day MA', linestyle='--', color='red')
+plt.plot(df.index, df['Second_Derivative_10_MA'], label='Second Derivative 10-day MA', linestyle='--', color='purple')
+plt.plot(df.index, df['Second_Derivative_50_MA'], label='Second Derivative 50-day MA', linestyle='--', color='pink')
+
 plt.xlabel('Date')
 plt.ylabel('Change')
-plt.title('First and Second Derivatives of "Adj Close"')
+plt.title('Derivatives and Moving Averages')
 plt.legend()
 plt.grid(True)
 plt.show()
